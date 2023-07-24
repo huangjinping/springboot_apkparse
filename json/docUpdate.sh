0932955d9ac1ea80fe96c3e45e9a1c85e2e2ac59
harris1000 <huangjinping1000@163.com>
2023-07-24 14:41:30 +0800

    33

--- json/docUpdate.sh
+++ json/docUpdate.sh
@@ -1,15 +1,15 @@
 #sh
-cd '/Users/huhuijie/Downloads/dist'
-echo "开始下载";
-ls;
-rm -rf 'inxdoc';
-git clone  'https://gitee.com/weiluTech/inxdoc.git'
-ls;
-rm -rf res;
-rm -rf src;
-rm doc.html;
-ls;
-mv ./inxdoc/doc.html doc.html
-mv ./inxdoc/res res
-mv ./inxdoc/src  src
-ls;
+#cd '/Users/huhuijie/Downloads/dist'
+#echo "开始下载";
+#ls;
+#rm -rf 'inxdoc';
+#git clone  'https://gitee.com/weiluTech/inxdoc.git'
+#ls;
+#rm -rf res;
+#rm -rf src;
+#rm doc.html;
+#ls;
+#mv ./inxdoc/doc.html doc.html
+#mv ./inxdoc/res res
+#mv ./inxdoc/src  src
+#ls;
